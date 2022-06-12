783a939b1671ec90c1d688991e2729df247731e1
Fabian Aguirre <aguirrefabian.unne@gmail.com>
2022-06-12 20:42:51 -0300

    add the exercise 3. The game of cards. Add the configuration in the home panel menu

--- Complementary_2/Complementary_2/src/Application_complementary2.java
+++ Complementary_2/Complementary_2/src/Application_complementary2.java
@@ -1,5 +1,6 @@
 import Complementary_2.Exercise1;
 import Complementary_2.Exercise2;
+import Complementary_2.Exercise3;
 
 import java.util.Scanner;
 
@@ -14,6 +15,7 @@ public class Application_complementary2 {
             System.out.println("Please, select one of exercise to perform:");
             System.out.println("(1) Exercise 1: list of favorite cities");
             System.out.println("(2) Exercise 2: list of numbers");
+            System.out.println("(3) Exercise 3: the cards game");
 
             System.out.println("(0) To Close the program");
             System.out.print("Your response: ");
@@ -31,6 +33,12 @@ public class Application_complementary2 {
                     System.out.println("-----------------");
                     System.out.println("Welcome to the exercise 2: list of numbers");
                     Exercise2.listOfNumbers();
+                    System.out.println("\n");
+                    break;
+                case 3:
+                    System.out.println("-----------------");
+                    System.out.println("Welcome to the exercise 3: the game of cards");
+                    Exercise3.cardGame();
                     System.out.println("\n");
                     break;
 
