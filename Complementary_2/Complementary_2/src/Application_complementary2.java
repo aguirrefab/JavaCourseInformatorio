02ca55a3fd875f8affbc8c7392d820242b74d0f6
Fabian Aguirre <aguirrefabian.unne@gmail.com>
2022-06-13 07:30:21 -0300

    Add the exercise number 6 class, and define the class Employee.

--- Complementary_2/Complementary_2/src/Application_complementary2.java
+++ Complementary_2/Complementary_2/src/Application_complementary2.java
@@ -1,6 +1,4 @@
-import Complementary_2.Exercise1;
-import Complementary_2.Exercise2;
-import Complementary_2.Exercise3;
+import Complementary_2.*;
 
 import java.util.Scanner;
 
@@ -16,6 +14,9 @@ public class Application_complementary2 {
             System.out.println("(1) Exercise 1: list of favorite cities");
             System.out.println("(2) Exercise 2: list of numbers");
             System.out.println("(3) Exercise 3: the cards game");
+            System.out.println("(4) Exercise 4: the group of students");
+            System.out.println("(5) Exercise 5: the report worked hours");
+            System.out.println("(6) Exercise 6: list and report of employees");
 
             System.out.println("(0) To Close the program");
             System.out.print("Your response: ");
@@ -41,8 +42,24 @@ public class Application_complementary2 {
                     Exercise3.cardGame();
                     System.out.println("\n");
                     break;
-
-
+                case 4:
+                    System.out.println("-----------------");
+                    System.out.println("Welcome to the exercise 4: groups of students");
+                    Exercise4.groupsOfStudents();
+                    System.out.println("\n");
+                    break;
+                case 5:
+                    System.out.println("-----------------");
+                    System.out.println("Welcome to the exercise 5: report of worked hours");
+                    Exercise5.controlHoursWorked();
+                    System.out.println("\n");
+                    break;
+                case 6:
+                    System.out.println("-----------------");
+                    System.out.println("Welcome to the exercise 6: list and report of employees");
+                    Exercise6.addEmployee();
+                    System.out.println("\n");
+                    break;
             }
         }while (selection != 0);
     }
