562038c4d3431e435878d5bb96422e68a62ed83b
Fabian Aguirre <aguirrefabian.unne@gmail.com>
2022-07-19 12:27:38 -0300

    Add the exercise 5 and the auxiliary class Students

--- Complementary_3/src/Exercise5.java
+++ Complementary_3/src/Exercise5.java
@@ -1,2 +1,30 @@
-package src;public class Exercise5 {
+package src;
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+
+public class Exercise5 {
+
+    public static void main(String[] args) {
+
+        // generate the list of students
+        List<Student> students = List.of(
+                new Student("Fabian", "Aguirre", LocalDate.of(1992,12,09)),
+                new Student("Homero", "Simpson",LocalDate.of(1959,03,12)),
+                new Student("Bartolomeo", "Simpson",LocalDate.of(1960,05,10)),
+                new Student("Frank", "Grimes",LocalDate.of(1975,07,30)),
+                new Student("Troy", "McClure",LocalDate.of(1988,10,02))
+        );
+
+        // create a map collection
+        Map<String,Integer> myStudents = new HashMap<>();
+
+        // create a Stream from the list of students
+//        students.stream()
+//                .forEach(student -> myStudents.put(student.getName(),student.getSurname(),
+//                ));
+
+}
 }
--- Complementary_3/src/Student.java
+++ Complementary_3/src/Student.java
@@ -1,2 +1,45 @@
-package src;public class Student {
+package src;
+
+import java.time.LocalDate;
+
+public class Student {
+    private String name;
+    private String surname;
+    private LocalDate birthday;
+
+    public Student(String name, String surname, LocalDate birthday) {
+        this.name = name;
+        this.surname = surname;
+        this.birthday = birthday;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getFullName(){
+        return this.name + " " + this.surname;
+    }
+
+
 }
