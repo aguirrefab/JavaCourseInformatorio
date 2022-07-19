dfb1f864194698832057405cd8fd961c11c2388d
Fabian Aguirre <aguirrefabian.unne@gmail.com>
2022-07-19 11:54:10 -0300

    add the exercise 4. Implement factorial function in Streams

---
+++ Complementary_3/src/Exercise4.java
@@ -0,0 +1,26 @@
+package src;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Exercise4 {
+
+    public static void main(String[] args) {
+        // Initial collection of numbers
+        List<Integer> numbers = List.of(1, 2, 4, 4, 4);
+
+        // create a stream and implemented the factorial function
+        Set<Integer> factorialNum = numbers.stream()
+                .map(number -> getFactorial(number))
+                .collect(Collectors.toSet());
+
+        System.out.println(factorialNum);
+
+    }
+    // refactor the factorial function
+    public static int getFactorial(int number) {
+        return (number <= 1) ? 1 : number * getFactorial(number - 1);
+    }
+}
